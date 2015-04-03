c4fdac35764ffe985078c4d4b71de9e5beab9a36
Steven Wichers <steven.wichers@gmail.com>
2015-04-03 16:05:37 -0700

    Make fish config a little more portable.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,9 @@
-set -gx EDITOR 'subl -w'
+if type -P 'subl'
+  set -gx EDITOR 'subl -w'
+else
+  set -gx EDITOR 'nano'
+end
+
 set -gx LANG en_US.UTF-8
 set -gx LC_CTYPE "en_US.UTF-8"
 set -gx LC_MESSAGES "en_US.UTF-8"
@@ -9,3 +14,5 @@ set -gx GIT_MERGE_AUTOEDIT no
 set -g default_user steven.wichers
 
 set -gx PATH $HOME/.composer/vendor/bin /usr/local/bin /usr/local/sbin $PATH
+
+. $HOME/.config/fish/config.local.fish
