0d46211b19d384fc4f13a38a8ad2ecfa0b409f83
Steven Wichers <me@stevenwichers.com>
2019-11-21 16:10:30 -0800

    Initial setup of YADM.

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+if type -P 'code' > /dev/null
+  set -gx EDITOR 'code'
+else
+  set -gx EDITOR 'nano'
+end
+
+set fish_greeting
+
+set -gx LANG en_US.UTF-8
+set -gx LC_CTYPE "en_US.UTF-8"
+set -gx LC_MESSAGES "en_US.UTF-8"
+set -gx LC_COLLATE C
+
+set -gx GIT_MERGE_AUTOEDIT no
+
+set -g default_user steven.wichers
+
+set -gx PATH $HOME/.composer/vendor/bin $HOME/.node/bin /usr/local/bin /usr/local/sbin $PATH
+set -gx PATH $HOME/.symfony/bin $PATH
+set -gx NODE_PATH $HOME/.node/lib/node_modules $NODE_PATH
+set -gx MANPATH $HOME/.node/share/man $MANPATH
+
+if test -e $HOME/.config/fish/config.local.fish
+  . $HOME/.config/fish/config.local.fish
+end
