22fc68f5f49f2145f9bee58a5ac33dd9c35ae8a6
Steven Wichers <me@stevenwichers.com>
2020-07-22 00:21:55 -0700

    Bring back some old prompt features.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,9 +1,74 @@
-function fish_prompt
-	test $SSH_TTY
-    and printf (set_color yellow)'⚡ '
-    test "$USER" = 'root'
-    and echo (set_color red)"#"
+set -g pad " "
 
-    # Main
-    echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+## Function to show a segment
+function prompt_segment -d "Function to show a segment"
+  # Get colors
+  set -l bg $argv[1]
+  set -l fg $argv[2]
+
+  # Set 'em
+  set_color -b $bg
+  set_color $fg
+
+  # Print text
+  if [ -n "$argv[3]" ]
+    echo -n -s $argv[3]
+  end
+end
+
+## Function to show current status
+function show_status -d "Function to show the current status"
+  if [ $RETVAL -ne 0 ]
+    prompt_segment normal normal " 💔 "
+    set pad ""
+  end
+
+  if [ -n "$SSH_TTY" ]
+    prompt_segment normal normal " 🔐 "
+    set pad ""
+  end
+end
+
+## Show user if not default
+function show_user -d "Show user"
+  if [ "$USER" != "$default_user" -o -n "$SSH_CLIENT" ]
+    set -l host (hostname -s)
+    set -l who (whoami)
+    prompt_segment normal yellow " $who"
+
+    # Skip @ bit if hostname == username
+    if [ "$USER" != "$HOST" ]
+      prompt_segment normal white "@"
+      prompt_segment normal green "$host "
+      set pad ""
+    end
+  end
+end
+
+# Show directory
+function show_pwd -d "Show the current directory"
+  set -l pwd (prompt_pwd)
+  prompt_segment normal cyan "$pwd "
+end
+
+# Show prompt w/ privilege cue
+function show_prompt -d "Shows prompt with cue for current priv"
+  set -l uid (id -u $USER)
+  if [ $uid -eq 0 ]
+    prompt_segment red white " !!! "
+  else
+    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯'
+  end
+
+  set_color normal
+  echo -n -s " "
+end
+
+
+function fish_prompt
+  set -g RETVAL $status
+  show_status
+  # show_user
+  show_pwd
+  show_prompt
 end
